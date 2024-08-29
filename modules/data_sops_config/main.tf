81981f33c485767b5af791ea68be0a02d0b2e788
Truong Hoang Phuong <thphuong1093@gmail.com>
2024-08-29 10:52:50 +0700

    feat: allow nested list in sops recipients

--- modules/data_sops_config/main.tf
+++ modules/data_sops_config/main.tf
@@ -7,7 +7,14 @@ data "local_file" "sops_config" {
 }
 
 locals {
-  dirname = dirname(var.config_path)
+  dirname     = dirname(var.config_path)
   config_yaml = yamldecode(data.local_file.sops_config.content)
+
   base_path = try("${local.dirname}/${trimprefix(local.config_yaml.base_path, "/")}", local.dirname)
+  paths = [
+    for p in local.config_yaml.paths : {
+      path_regex = p.path_regex
+      recipients = flatten(p.recipients) # Flatten recipients so anchors can be used
+    }
+  ]
 }
--- modules/data_sops_config/outputs.tf
+++ modules/data_sops_config/outputs.tf
@@ -1,5 +1,7 @@
 output "config" {
-  value = merge(local.config_yaml, {
-    base_path = local.base_path
-  })
+  value = {
+    base_path  = local.base_path
+    paths      = local.paths
+    recipients = local.config_yaml.recipients
+  }
 }
